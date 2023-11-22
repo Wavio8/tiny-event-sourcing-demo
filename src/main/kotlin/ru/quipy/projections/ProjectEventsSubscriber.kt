33ad79afbbe9546adb5a2c9ca49b314b2e479fd7
DariaMakh <makhortova.daria@ya.ru>
2023-11-22 17:58:57 +0300

    fix: subscriber

--- src/main/kotlin/ru/quipy/projections/ProjectEventsSubscriber.kt
+++ src/main/kotlin/ru/quipy/projections/ProjectEventsSubscriber.kt
@@ -4,11 +4,10 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import ru.quipy.api.ProjectAggregate
-import ru.quipy.api.TagAssignedToTaskEvent
-import ru.quipy.api.TagCreatedEvent
-import ru.quipy.api.TaskCreatedEvent
+import ru.quipy.api.*
+import ru.quipy.projections.views.UserViewDomain
 import ru.quipy.streams.AggregateSubscriptionsManager
+import java.util.*
 import javax.annotation.PostConstruct
 
 @Service
@@ -23,8 +22,18 @@ class ProjectEventsSubscriber {
     fun init() {
         subscriptionsManager.createSubscriber(ProjectAggregate::class, "project-subscriber") {
 
-            `when`(TaskCreatedEvent::class) { event ->
+            `when`(ProjectCreatedEvent::class) { event ->
+                createUser(event.userId, event.username)
                 logger.info("Task created: {}", event.taskName)
+            }
+
+            `when`(TaskCreatedEvent::class) { event ->
+                createUser(event.userId, event.username)
+                logger.info("Task created: {}", event.taskName)
+            }
+
+            `when`(TagCreatedEvent::class) { event ->
+                logger.info("Tag created: {}", event.tagName)
             }
 
             `when`(TagCreatedEvent::class) { event ->
@@ -35,5 +44,18 @@ class ProjectEventsSubscriber {
                 logger.info("Tag {} assigned to task {}: ", event.tagId, event.taskId)
             }
         }
+    }
+    private fun createProject(userId: UUID, userName: String) {
+        val user = UserViewDomain.User(userId, userName)
+        //Repository.save(user)
+    }
+    private fun createUser(userId: UUID, userName: String) {
+        val user = UserViewDomain.User(userId, userName)
+        //Repository.save(user)
+    }
+
+    private fun changeUserName(userId: UUID, userName: String) {
+        val user = UserViewDomain.User(userId, userName)
+        //Repository.update(user)
     }
 }
